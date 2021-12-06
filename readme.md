b1438c30ace924cfb141cba6ffc429aece320dd8
Vitor <joaogauglitz@gmail.com>
2021-12-06 16:09:31 -0300

    pj1

--- readme.md
+++ readme.md
@@ -1,27 +1,26 @@
 ## FinAPI - Financeira
 
-
-//Jaja vou subir aqui completo//
 ---
 
 ### Requisitos 
 
-- [] Deve ser possível criar uma conta
-- [] Deve ser possível buscar o extrato bancário do cliente
-- [] Deve ser possível realizar um deposito
-- [] Deve ser possível realizar um saque 
-- [] Deve ser possível buscar o extrato bancário do cliente por data
-- [] Deve ser possível atualizar dados da conta do cliente
-- [] Deve ser possível obter dados da conta do cliente
-- [] Deve ser possível deletar uma conta
+- [x] Deve ser possível criar uma conta
+- [x] Deve ser possível buscar o extrato bancário do cliente
+- [x] Deve ser possível realizar um deposito
+- [x] Deve ser possível realizar um saque 
+- [x] Deve ser possível buscar o extrato bancário do cliente por data
+- [x] Deve ser possível atualizar dados da conta do cliente
+- [x] Deve ser possível obter dados da conta do cliente
+- [x] Deve ser possível deletar uma conta
+- [x] Deve ser possível retornar o balance
 
 ---
 
 ## Regras de negócio
 
-- [] Não deve ser possível cadastrar umma conta com CPF já existente
-- [] Não deve ser possível fazer um depósito em uma conta não existente
-- [] Não deve ser possível buscar extrato em uma conta não existente
-- [] Não deve ser possível fazer um saque em uma conta não existente 
-- [] Não deve ser possível excluir uma conta não existente
-- [] Não deve ser possível fazer saque quando o saldo for insuficiente
+- [x] Não deve ser possível cadastrar umma conta com CPF já existente
+- [x] Não deve ser possível buscar extrato em uma conta não existente
+- [x] Não deve ser possível fazer um depósito em uma conta não existente
+- [x] Não deve ser possível fazer um saque em uma conta não existente 
+- [x] Não deve ser possível fazer saque quando o saldo for insuficiente
+- [x] Não deve ser possível excluir uma conta não existente
--- src/index.js
+++ src/index.js
@@ -8,12 +8,32 @@ app.use(express.json()) //mdware
 
 const customers = []
 
-/*
-CPF string
-name - string
-id uuid
-statement []
-*/
+//Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = req.headers
+
+    const customer = customers.find((customer) => customer.cpf === cpf)
+
+    if(!customer){
+        return res.status(400).json({ error: "Customer not found"})
+    }
+
+    request.customer = customer
+
+    return next()
+}
+
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === "credit") {
+            return acc + operation.amount
+        }else {
+            return acc - operation.amount
+        }
+    }, 0)
+
+    return balance
+}
 
 app.post("/account", (req, res) => {
     const { cpf, name } = req.body //desestruturação
@@ -34,5 +54,88 @@ customers.push({
 
 })
 
+app.get("/statement", verifyIfExistsAccountCPF , (req, res) => {
+    const { customer } = req
+
+    return res.json(customer.statement)
+})
+
+app.post("/deposit", verifyIfExistsAccountCPF, (req, res) => {
+    const { description, amount } = req
+
+    const { customer } = req
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation)
+
+    return res.status(201).send()
+})
+
+app.post("/withdraw", verifyIfExistsAccountCPF,(req, res) => {
+    const { amount } = req.body
+    const { customer } = req
+
+    const balance = getBalance(customer.statement)
+    if(balance < amount) {
+        return res.status(400).json({ error: "Insufficient founds!"})
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    }
+
+    customer.statement.push(statementOperation)
+
+    return res.status(201).send()
+})
+
+app.get("/statement/date", verifyIfExistsAccountCPF , (req, res) => {
+    const { customer } = req
+    const { date } = req.query
+
+    const dateFormat = new Date(date + " 00:00")
+
+    const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+
+    return res.json(customer.statement)
+})
+
+app.put("/account", verifyIfExistsAccountCPF , (req, res) => {
+    const { name } = req.body
+    const { customer } = req
+
+    customer.name = name
+
+    return res.status(201).send()
+})
+
+app.get("/account", verifyIfExistsAccountCPF , (req, res) => {
+    const { customer } = req
+    
+    return res.json(customer)
+})
+
+app.delete("/account", verifyIfExistsAccountCPF, (req, res) => {
+    const { customer } = req
+
+    customers.splice(customer, 1)
+    return res.status(200)
+})
+
+app.get("/balance", verifyIfExistsAccountCPF, (req, res) => {
+    const { customer } = req
+
+    const balance = getBalance(customer.statement)
+
+    return res.json(balance)
+})
 
 app.listen(3333)
